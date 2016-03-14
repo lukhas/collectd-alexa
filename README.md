3acb5f264e84cf87b39c26983e67de35750474d9
Lucas Bonnet <lbonnet@bearstech.com>
2016-03-14 22:44:47 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # collectd-alexa
 Alexa plugin for Lichess collectd
+
+Copied Alexa Munin plugin written by Clarkey from
+https://github.com/ornicar/munin-lichess and adapted for Collectd
---
+++ alexa-plugin.php
@@ -0,0 +1,48 @@
+#!/usr/bin/env php
+<?php
+
+// set variables
+$hostname=getenv('COLLECTD_HOSTNAME');
+
+if ($hostname == FALSE) {
+  $hostname = rtrim(`hostname -f`);
+}
+
+$interval=getenv('COLLECTD_INTERVAL');
+
+if ($interval == FALSE) {
+  $interval = 60;
+}
+
+$sites = array(
+    "chessbomb.com",
+    "lichess.org",
+    "chess24.com",
+    "chesscube.com",
+    "gameknot.com",
+    "itsyourturn.com",
+    "sparkchess.com",
+    "chessbase.com",
+    "chesstempo.com",
+    "chess.com");
+
+$chess_links = "";
+$str = "";
+
+function safe($name) {
+    return str_replace('.', '_', $name);
+}
+
+while (TRUE) {
+  $chess_links = file_get_contents("http://chess-links.org/index.html");
+  
+  foreach ($sites as $site) {
+    preg_match("/($site).+?<td>([0-9]+)<\/td>/", $chess_links, $val);
+    $str = "PUTVAL \"$hostname/alexa/counter-" . safe($site) . "\" interval=$interval N:" .$val[2] ."\n";
+    // print_r(safe($site) . ".value ".$val[2]."\n");
+    print_r($str);
+  }
+  sleep($interval);
+}
+
+?>
